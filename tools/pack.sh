e752fceb87a57ec6e1413bc11d3941393f08d60d
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-11-26 09:41:40 +0300

    upd - Updated workflows for .NET 8.0

--- tools/pack.sh
+++ tools/pack.sh
@@ -17,7 +17,7 @@ fi
 # Pack binary
 echo Packing binary...
 cd "../Yunikodo/bin/$releaseconf/netstandard2.0/" && "$zippath" -r /tmp/$version-bin.zip . && cd -
-cd "../Yunikodo.QueryApp/bin/$releaseconf/net6.0/" && "$zippath" -r /tmp/$version-bin.zip . && cd -
+cd "../Yunikodo.QueryApp/bin/$releaseconf/net8.0/" && "$zippath" -r /tmp/$version-bin.zip . && cd -
 if [ ! $? == 0 ]; then
 	echo Packing using zip failed.
 	exit 1
